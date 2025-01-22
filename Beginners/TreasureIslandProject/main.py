6e018cf3c8957942db62f7c6bd71471b854a74fe
rup-raj <rajrupeshadit@gmail.com>
2025-01-22 19:44:09 +0530

    Added new project : Treasure Island Project

---
+++ Beginners/TreasureIslandProject/main.py
@@ -0,0 +1,44 @@
+# https://app.diagrams.net/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+direction = input("\nDo you want to take right of left?")
+
+if direction.lower() == "left":
+    choice = input("\nThere is lake ahead, Do you want to swim or wait?")
+    if choice.lower() == "wait":
+        door = input("\nThere is 3 doors red, yellow and blue. select any one.")
+        if door.lower() == "red":
+            print("\nYou are burned by fire. Game over...")
+        elif door.lower() == "blue":
+            print("\nYou are eaten by beasts. Game over...")
+        elif door.lower() == "yellow":
+            print("\nYou did it! You have found the treasure. Congratulations")
+        else:
+            print("\nGame over...")
+    else:
+        print("\nYou are attacked by the trout. Game over...")
+else:
+    print("\nFall into a hole. Game over...")
