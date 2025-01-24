da46acf109781cb9a516ad908e46aff5919c0f0c
rup-raj <rajrupeshadit@gmail.com>
2025-01-24 11:33:34 +0530

    Update the logic: Rock Paper Scissors

--- Beginners/RockPaperScissors/main.py
+++ Beginners/RockPaperScissors/main.py
@@ -31,7 +31,8 @@ sign = {"rock" : '''
 ---.__(___)
 '''}
 
-winning_combo = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
+winning_combination_dict = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
+winning_combination_tuple = [("rock", "scissors"), ("paper", "rock"), ("scissors", "paper")]
 
 def display(user, computer):
     if user in list(sign.keys()):
@@ -42,10 +43,22 @@ def display(user, computer):
         return 0
 
 
-def result(u, c):
+def result_using_dictionary(u, c):
+    # Pros: For rock paper scissors game dictionary based approach is good because there one-to-one relationship in winning combination.
+    # Cons: It is not scalable because if we want to extend this game (ex. rock paper scissors lizard spock)
     if u == c:
         print("Game Draw!")
-    elif winning_combo[u] == c:
+    elif winning_combination_dict[u] == c:
+        print("You Win!")
+    else:
+        print("Computer Wins!")
+
+
+def result_using_tuple(u1,c1):
+    # Pros: It is simple and easy to extend approach. We can easily add new rules in the list of tuples
+    if u1==c1:
+        print("Game Draw!")
+    elif (u1, c1) in winning_combination_tuple:
         print("You Win!")
     else:
         print("Computer Wins!")
@@ -53,8 +66,9 @@ def result(u, c):
 
 if __name__ == "__main__":
     user_choice = input("What do you choose? Rock, Paper and Scissors: ").lower()
-    computer_choice = random.choice(list(winning_combo.keys()))
+    computer_choice = random.choice(list(winning_combination_dict.keys()))
     if display(user_choice, computer_choice):
-        result(user_choice, computer_choice)
+        #result_using_dictionary(user_choice, computer_choice)
+        result_using_tuple(user_choice, computer_choice)
     else:
         print("Invalid Choice!")
